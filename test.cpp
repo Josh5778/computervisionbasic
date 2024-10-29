07e4d2f58f573054fcd0e6f4869d96fabbc591dd
Josh5778 <joshvalastro2005@gmail.com>
2024-10-30 00:30:19 +1100

    Add project files.

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+void main() {
+	string path = "";
+
+}
