2bbd02a452f7e940348a501dc7118a620c612c9d
Josh5778 <joshvalastro2005@gmail.com>
2024-10-30 11:56:17 +1100

    finished product

--- test.cpp
+++ test.cpp
@@ -7,10 +7,42 @@
 using namespace std;
 using namespace cv;
 
-void main() {
-	string path = "";
-	Mat img = imread(path);
-	imshow("Frame", img);
-	waitKey(0);
+//int main() {
+//	string path = "keanu_reeves.jpg";
+//	Mat img = imread(path);
+//	imshow("Frame", img);
+//	waitKey(0);
+//
+//}
 
+int main() {
+	try {
+		VideoCapture video(0);
+		CascadeClassifier facedetect;
+		Mat img;
+		facedetect.load("haarcascade_frontalface_default.xml");
+
+		while (true) {
+
+			video.read(img);
+
+			vector<Rect> faces;
+
+			facedetect.detectMultiScale(img, faces, 1.3, 5);
+
+			cout << faces.size() << endl;
+
+			for (int i = 0; i < faces.size(); i++) {
+				rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+				putText(img, to_string(faces.size()), Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
+			} 
+
+			imshow("Frame", img);
+			waitKey(1);
+		}
+	}
+	catch (const cv::Exception& e) {
+		std::cerr << "OpenCV error: " << e.what() << std::endl;
+	}
 }
+
